cf6cab36656fab8f48ced1dbdf9b5c676e5f5132
ElianoPereira <ejeulin@outlook.fr>
2021-07-12 18:18:38 +0200

    Initial commit

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float cooldawn = 1.5f;
+    private float timePassed = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && timePassed > cooldawn)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            timePassed = 0;
+        }
+
+        timePassed += Time.deltaTime;
+    }
+}
